b5b3c367f47ae2160b0ea73dbb49ac9e70720cdf
Julian Fontecha <jfontech@uncc.edu>
2017-06-08 22:43:51 +0000

    2.0: App is ready for distribution. Had Barry Chau change the HTML and CSS for better presentation. Current contributors: Barry Chau, Julian Fontecha, Victor Viteri

--- event.js
+++ event.js
@@ -1,14 +1,15 @@
+
 function deduction(info,tab) {
   console.log(info.selectionText + " was clicked.");
 
    
-		var input = info.selectionText;
-    	if(/^\d+(\.\d+)?$/.test(input))
+		var input = info.selectionText; //highlighted input from context menu
+    	if(/^\d+(\.\d+)?$/.test(input)) //if the input is a float number
     	{
 			//chrome.tabs.create({  url:
 			//"tab.html",
 			//});   
-			deduct(input);								
+			deduct(input);	//subtraction logic						
     	}
     	else
     	{
@@ -21,13 +22,13 @@ function addition(info,tab) {
   console.log(info.selectionText + " was clicked.");
 
    
-		var plus = info.selectionText;
-    	if(/^\d+(\.\d+)?$/.test(plus))
+		var plus = info.selectionText; //highlighted input from context menu
+    	if(/^\d+(\.\d+)?$/.test(plus)) //if the input is a float number
     	{
 			//chrome.tabs.create({  url:
 			//"tab.html",
 			//});   
-			add(plus);								
+			add(plus);	//addition logic						
     	}
     	else
     	{
@@ -37,34 +38,39 @@ function addition(info,tab) {
 }
 
 function deduct(input){	
-var inp = parseFloat(input);
-var _budget = localStorage.getItem("bName");
-var	nBudget = _budget - inp;
-localStorage.setItem("bName", nBudget);
-
+var inp = parseFloat(input); //convert string to float
+var _budget = localStorage.getItem("bName"); //call local storage variable of budget
+var	nBudget = _budget - inp; //new budget
+localStorage.setItem("bName", nBudget); // save new budget in local storage
+if(nBudget <= 0)
+{
+	alert("You just went over your budget"); //alert for when the budget goes below 0
+}
 }
 
 function add(plus){	
-var pl = parseFloat(plus);
-var _budget = localStorage.getItem("bName");
-var budget = parseFloat(_budget);
-var	newBudget = budget + pl;
-localStorage.setItem("bName", newBudget);
+var pl = parseFloat(plus); //convert string to float
+var _budget = localStorage.getItem("bName"); //call local storage variable of budget
+var budget = parseFloat(_budget); //convert string to float
+var	newBudget = budget + pl; // new budget
+localStorage.setItem("bName", newBudget); // save new budget in local storage
 
 }
 
-chrome.contextMenus.removeAll(function() {
-
+//context menu for the deduction
 chrome.contextMenus.create({
   title: "Deduct from budget", 
+  id: "subtract",
   contexts:["selection"], 
   onclick: deduction,
 });
 
+//context menu for the addition
 chrome.contextMenus.create({
   title: "Put item back", 
+  id: "addthis",
   contexts:["selection"], 
   onclick: addition,
 });
 
-});
+
--- popup.js
+++ popup.js
@@ -5,13 +5,15 @@ document.addEventListener('DOMContentLoaded', function() {
         sendForm();
 		
     });
+    //Display current budget when extension loads
 	var _budget = localStorage.getItem("bName");
     document.getElementById("bOutput").innerHTML = "$" + _budget;
 });
 
+//onClick logic of 'send' button
 function sendForm()
 {
-    var z = document.getElementById('budget').value;
+    var z = document.getElementById('budget').value; //budget input
     
     if (z=="")
     {
@@ -19,7 +21,7 @@ function sendForm()
         document.getElementById('bg').innerHTML="Please enter a budget"; 
     }
     else         
-		if(/^\d+(\.\d+)?$/.test(z))
+		if(/^\d+(\.\d+)?$/.test(z)) //If the input is an integer or a decimal
 		{
 			proceed(z);
 			return false;
@@ -34,7 +36,7 @@ function sendForm()
 
 function proceed(z)
 {
-	 var s = parseFloat(z);
-    localStorage.setItem("bName", s);     
-    window.location = "userBudget.html";
+	 var s = parseFloat(z); //Turn input into a float value
+    localStorage.setItem("bName", s); //Make a local storage variable with the budget input     
+    window.location = "userBudget.html"; //Load the userBudget page
 }
